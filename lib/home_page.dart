113ff7dcce30db3d17420959b46f609c9255b5eb
mohdi <mohdinzamam12125@gmail.com>
2024-05-07 21:28:49 +0530

    TO DO App 2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,94 +1,54 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:to_do_app/core/database/local_database.dart';
-import 'package:to_do_app/widgets/ToDo_tile.dart';
-import 'package:to_do_app/widgets/dialog_box.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final _controller = TextEditingController();
-
-  // reference
-  final _myBox = Hive.box('myBox');
-
-  LocalDatabase db = LocalDatabase();
-
-  void checkBoxChanged(bool value, int index){
-    setState(() {
-      db.listTODO[index][1] = !db.listTODO[index][1];
-    });
-    db.updateDatabase();
-  }
-
-  void saveNewTask(){
-    setState(() {
-      db.listTODO.add([_controller.text, false]);
-      _controller.clear();
-      Navigator.pop(context);
-    });
-    db.updateDatabase();
-  }
-
-  void createNewTask(){
-    showDialog(context: context,
-        builder: (context){
-      return DialogBox(
-        controller: _controller,
-        onSave: saveNewTask,
-      );
-        });
-  }
-
-  void deleteTask(int index){
-    setState(() {
-      db.listTODO.removeAt(index);
-    });
-    db.updateDatabase();
-  }
-
-  @override
-  void initState() {
-    if(_myBox.get("TODOLIST") == null){
-      db.createInitialData();
-    }else{
-      db.loadData();
-    }
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow.shade200,
-      floatingActionButton: FloatingActionButton(
-        onPressed: createNewTask,
-        backgroundColor: Colors.yellow,
-        child: const Icon(Icons.add),
-      ),
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text('TO DO'),
-        shadowColor: Colors.black,
-        elevation: 2,
+        title: Container(
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Text("23 MAY 2023",
+            style: TextStyle(
+              fontWeight: FontWeight.w700
+            ),)
+              , Text("TUESDAY",
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.grey
+              ),)],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: PhysicalModel(
+              color: Colors.white,
+              elevation: 3,
+              borderRadius: BorderRadius.circular(30),
+              child: IconButton(
+                onPressed: () {},
+                icon: Transform.flip(
+                    flipX: true,
+                    child: const Icon(Icons.dark_mode_outlined)),
+              ),
+            ),
+          )
+        ],
       ),
       body: Container(
-        child: ListView.builder(
-          itemCount: db.listTODO.length,
-            itemBuilder: (context,index){
-            return ToDoTile(
-              taskName: db.listTODO[index][0],
-              taskCompleted: db.listTODO[index][1],
-              onChanged: (value) => checkBoxChanged(value!, index),
-              deleteFunction: (context) => deleteTask(index),
-            );
-            }),
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Text("To Do",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20
+            ),)
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:to_do_app/home_page.dart';
-import 'design/app_theme.dart';
 
-void main() async{
-  await Hive.initFlutter();
-  var box = await Hive.openBox('myBox');
+void main(){
   runApp(const MyApp());
 }
 
@@ -14,10 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightThemeColor,
-      home: HomePage()
+      home: HomePage(),
     );
   }
 }
